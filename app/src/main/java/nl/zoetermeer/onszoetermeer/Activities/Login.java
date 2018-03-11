b10475332227d21e6204026686150dfd77fa4003
Ken <k.dillewaard@hotmail.com>
2018-03-11 14:52:09 +0100

    -added InputValidator helper class -added basic validation to Login&Registration

--- app/src/main/java/nl/zoetermeer/onszoetermeer/Activities/Login.java
+++ app/src/main/java/nl/zoetermeer/onszoetermeer/Activities/Login.java
@@ -3,11 +3,15 @@ package nl.zoetermeer.onszoetermeer.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import nl.zoetermeer.onszoetermeer.Data.DummyDatabase;
+import nl.zoetermeer.onszoetermeer.Helpers.InputValidator;
 import nl.zoetermeer.onszoetermeer.Models.User;
 import nl.zoetermeer.onszoetermeer.R;
 
@@ -15,6 +19,9 @@ import nl.zoetermeer.onszoetermeer.R;
 public class Login extends AppCompatActivity
 {
     private DummyDatabase dummyDB;
+    private InputValidator inputValidator;
+    private boolean validationStatus;
+    EditText loginEmail, loginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,14 @@ public class Login extends AppCompatActivity
         Log.i("ACTIVITY:", "Login created.");
 
         dummyDB = DummyDatabase.getDatabase(getApplicationContext());
+        inputValidator = new InputValidator();
 
+        validationStatus = false;
+
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_email);
+
+        addListeners();
 
     }
 
@@ -55,5 +69,27 @@ public class Login extends AppCompatActivity
                 setContentView(R.layout.activity_login_screen);
             }
         }
+    }
+
+    public void addListeners() {
+
+        loginEmail.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                inputValidator.validateEmail(loginEmail);
+            }
+        });
+
     }
 }
--- app/src/main/java/nl/zoetermeer/onszoetermeer/Activities/Registration.java
+++ app/src/main/java/nl/zoetermeer/onszoetermeer/Activities/Registration.java
@@ -2,6 +2,8 @@ package nl.zoetermeer.onszoetermeer.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -11,15 +13,19 @@ import android.widget.Toast;
 import java.util.Date;
 
 import nl.zoetermeer.onszoetermeer.Data.DummyDatabase;
+import nl.zoetermeer.onszoetermeer.Helpers.InputValidator;
 import nl.zoetermeer.onszoetermeer.Models.User;
 import nl.zoetermeer.onszoetermeer.R;
 
 public class Registration extends AppCompatActivity
 {
     private DummyDatabase dummyDB;
-    private User newUser = new User();
-    private boolean genderStatus = false;
-    private boolean validationStatus = false;
+    private InputValidator inputValidator;
+    private User newUser;
+    private boolean genderStatus;
+    private boolean validationStatus;
+    private EditText regEmail, regFname, regLname, regPw1, regPw2;
+    private RadioGroup regGndr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,16 +35,84 @@ public class Registration extends AppCompatActivity
         Log.i("ACTIVITY:", "Registration created.");
 
         dummyDB = DummyDatabase.getDatabase(getApplicationContext());
+        inputValidator = new InputValidator();
+        newUser = new User();
+        genderStatus = false;
+        validationStatus = false;
 
+        regEmail = findViewById(R.id.reg_input_mail);
+        regFname = findViewById(R.id.reg_input_fname);
+        regLname = findViewById(R.id.reg_input_lname);
+        regGndr = findViewById(R.id.gender);
+        regPw1 = findViewById(R.id.reg_input_pw1);
+        regPw2 = findViewById(R.id.reg_input_pw2);
 
+        addListeners();
         checkGender();
 
     }
 
+
+    public void addListeners() {
+
+        regEmail.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                inputValidator.validateEmail(regEmail);
+            }
+        });
+
+        regFname.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                inputValidator.validateName(regFname);
+            }
+        });
+
+        regLname.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                inputValidator.validateName(regLname);
+            }
+        });
+    }
+
     public void checkGender() {
 
-        final RadioGroup radioGroup = findViewById(R.id.gender);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        regGndr.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 switch (checkedId) {
@@ -58,27 +132,26 @@ public class Registration extends AppCompatActivity
                 }
             }
         });
+
     }
+
 
     public void validateRegistration(View view) {
 
-        //TO-DO validation code
+        //TO-DO check validator statusses
 
         createUser(view);
     }
 
     public void createUser(View view) {
 
-        EditText reg_email = findViewById(R.id.reg_input_mail);
-        EditText reg_fname = findViewById(R.id.reg_input_fname);
-        EditText reg_lname = findViewById(R.id.reg_input_lname);
 
-        EditText reg_pswrd = findViewById(R.id.reg_input_pw1);
+        newUser.setM_email(regEmail.getText().toString());
+        newUser.setM_first_name(regFname.getText().toString());
+        newUser.setM_last_name(regLname.getText().toString());
 
-        newUser.setM_email(reg_email.getText().toString());
-        newUser.setM_first_name(reg_fname.getText().toString());
-        newUser.setM_last_name(reg_lname.getText().toString());
-        newUser.setM_password(reg_pswrd.getText().toString());
+        // TO-DO get password from validator
+//        newUser.setM_password(regPw1.getText().toString());
 
         Date date = new Date();
         newUser.setM_last_active(date);
@@ -88,7 +161,7 @@ public class Registration extends AppCompatActivity
         Log.i("DATABASE:", "New User created");
 
         //check of gebruiker is aangemaakt
-        User user = dummyDB.userDAO().getByName(reg_fname.getText().toString()).get(0);
+        User user = dummyDB.userDAO().getByName(regFname.getText().toString()).get(0);
         Toast.makeText(this, String.valueOf("Gebruiker " + user.getM_first_name() + " aangemaakt"), Toast.LENGTH_LONG).show();
     }
 }
