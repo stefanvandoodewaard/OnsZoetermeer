24c2f71cda7e6fe1eff32ad0e7e2052c36e79fa8
svandoodewaard@gmail.com <L3PQrA4396RnS>
2018-03-18 14:57:51 +0100

    Add's: - Progressbar's fully working on home screen - Images progressbar's - Percentage's progressbar's

--- app/src/main/java/nl/zoetermeer/onszoetermeer/Activities/Home.java
+++ app/src/main/java/nl/zoetermeer/onszoetermeer/Activities/Home.java
@@ -1,9 +1,12 @@
 package nl.zoetermeer.onszoetermeer.Activities;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -12,6 +15,10 @@ import android.util.Log;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
 
 import nl.zoetermeer.onszoetermeer.R;
 
@@ -19,26 +26,144 @@ public class Home extends AppCompatActivity
 {
     private DrawerLayout mDrawerLayout;
 
+    int pStatusMentaal = 0;
+    int pStatusFysiek = 0;
+    TextView percentageMentaal;
+    TextView percentageFysiek;
+    private Handler handler = new Handler();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        drawMentalProgress();
+        drawPhysical();
+        drawToolbar();
+    }
+
+    private void drawPhysical()
+    {
+        Drawable drawablePhysical = ResourcesCompat.getDrawable(getResources(),
+                R.drawable.progressbarstyle, null);
+
+        final ProgressBar progressFysiek = findViewById(R.id.circularProgressbarPhysical);
+
+        progressFysiek.setProgress(100);
+        progressFysiek.setSecondaryProgress(100);
+        progressFysiek.setMax(100);
+        progressFysiek.setProgressDrawable(drawablePhysical);
+
+        percentageFysiek = findViewById(R.id.percentageFysiek);
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                double progressUserPhysical = 30;
+
+                while (pStatusFysiek < progressUserPhysical)
+                {
+                    pStatusFysiek += 1;
+
+                    handler.post(new Runnable()
+                    {
+
+                        @Override
+                        public void run()
+                        {
+                            progressFysiek.setProgress(pStatusFysiek);
+                            percentageFysiek.setText(MessageFormat.format("{0}%",
+                                    pStatusFysiek));
+                        }
+                    });
+
+                    try
+                    {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void drawMentalProgress()
+    {
+        Drawable drawableMental = ResourcesCompat.getDrawable(getResources(),
+                R.drawable.progressbarstyle, null);
+
+        final ProgressBar mProgressMentaal = findViewById(R.id.circularProgressbarMental);
+
+        mProgressMentaal.setProgress(100);
+        mProgressMentaal.setSecondaryProgress(100);
+        mProgressMentaal.setMax(100);
+        mProgressMentaal.setProgressDrawable(drawableMental);
+
+        percentageMentaal = findViewById(R.id.percentageMentaal);
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                double progressUserMental = 60;
+
+                while (pStatusMentaal < progressUserMental)
+                {
+                    pStatusMentaal += 1;
+
+                    handler.post(new Runnable()
+                    {
+
+                        @Override
+                        public void run()
+                        {
+                            mProgressMentaal.setProgress(pStatusMentaal);
+                            percentageMentaal.setText(MessageFormat.format("{0}%",
+                                    pStatusMentaal));
+                        }
+                    });
+
+                    try
+                    {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void drawToolbar()
+    {
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
+
         ActionBar actionbar = getSupportActionBar();
+
+        assert actionbar != null;
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
         actionbar.setDisplayShowTitleEnabled(false);
 
-
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
+                new NavigationView.OnNavigationItemSelectedListener()
+
+                {
                     @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+                    {
                         // set item as selected to persist highlight
                         menuItem.setChecked(true);
                         // close drawer when item is tapped
@@ -53,8 +178,10 @@ public class Home extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
                 return true;
